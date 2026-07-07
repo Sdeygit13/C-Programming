bb41626c6a52042570f4899943afe4f14aa88646
SUVAM DEY <deysuvampocom06@gmail.com>
2026-07-07 11:21:17 +0530

    Add Ackermann function implementation
    Implemented the Ackermann function for recursive computation.

---
+++ Function & Pointer/Ackermann_Recursion.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+int ackermann(int m, int n)
+{
+    if(m == 0)
+        return n + 1;
+
+    if(n == 0)
+        return ackermann(m - 1, 1);
+
+    return ackermann(m - 1, ackermann(m, n - 1));
+}
+
+int main()
+{
+    int m, n;
+
+    printf("Enter m and n: ");
+    scanf("%d%d",&m,&n);
+
+    printf("Ackermann Function = %d", ackermann(m, n));
+
+    return 0;
+}
