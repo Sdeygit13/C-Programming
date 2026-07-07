73230c776d3c4302c7d325a9d66cf61487d881b4
Suvam Dey <deysuvampocom06@gmail.com>
2026-07-07 22:16:20 +0530

    Add LengthOfString.c to calculate string length

---
+++ String/LengthOfString.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char str[60];
+
+    printf("Enter a string: ");
+    scanf("%s",str);
+
+    printf("The length is : %d",strlen(str));
+
+    return 0;
+}
